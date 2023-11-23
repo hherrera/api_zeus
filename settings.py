a5b425252f754adb24b879b03a7d6b0a6bb27085
hherrera <hherrera@araujoysegovia.com>
2023-11-23 16:46:16 -0500

    supabase, orders, invoices

--- settings.py
+++ settings.py
@@ -16,10 +16,19 @@ class Settings:
         self.PASSWORD:str = os.environ.get("PASSWORD")
         self.PORT:str = os.environ.get("PORT")
         self.DATABASE:str = os.environ.get("DATABASE")
+        self.DB_INVENTARIO:str = os.environ.get("DB_INVENTARIO")
+        self.DB_NOMINA:str = os.environ.get("DB_NOMINA")
+        self.DB_CONTABILIDAD:str = os.environ.get("DB_CONTABILIDAD")
+        
         self.DRIVER:str = os.environ.get("DRIVER")
 
-    
+        ## Supabase 
+        self.SUPABASE_URL=os.environ.get("SUPABASE_URL")
+        self.SUPABASE_KEY=os.environ.get("SUPABASE_KEY")
+
     def database_url(self):
        return  f"Driver={self.DRIVER};Server={self.SERVER};PORT={self.PORT};DATABASE={self.DATABASE};UID={self.USER};pwd={self.PASSWORD};TDS_Version=7.2;CHARSET=UTF-8;"
-   
+    def set_database(self,dbname:str):
+        self.DATABASE:str=dbname
+
 settings = Settings()
